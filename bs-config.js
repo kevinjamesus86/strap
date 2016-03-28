f5f5096d26fcc4beaa8a3b7a59df639c2c23db6e
Kevin James <kevinjamesus86@gmail.com>
2016-03-27 18:08:00 -0700

    improve handling of less files
    print compilation errors to the console & send the message to the
    client in the css file

--- bs-config.js
+++ bs-config.js
@@ -2,21 +2,8 @@
 
 var fs = require('fs'),
     url = require('url'),
-    less = require('less');
-
-/**
- * Compile less
- */
-function parseLess(src, done) {
-    var config = {
-        paths: [
-            'app/src'
-        ]
-    };
-    fs.readFile('./' + src, 'utf-8', function(e, file) {
-        less.render(file, config).then(done, done);
-    });
-}
+    less = require('less'),
+    chalk = require('chalk');
 
 /**
  * Run the middleware on files that contain .less
@@ -29,8 +16,38 @@ function lessMiddleware(req, res, next) {
             res.end(o.css);
         });
     } else {
-        next();
+        next && next();
     }
+}
+
+/**
+ * Compile less
+ */
+function parseLess(src, done) {
+    var config = {
+        paths: [
+            'app/src',
+            'app/components'
+        ]
+    };
+
+    function nope(err) {
+        var msg =
+            '\n' +
+            chalk.red('LESS Compilation Error ') +
+            chalk.grey(JSON.stringify(err, null, 2)) +
+            '\n';
+
+        console.log(msg);
+
+        done({
+            css:`/** ${chalk.stripColor(msg)} */`
+        });
+    }
+
+    fs.readFile('./' + src, 'utf-8', function(e, file) {
+        less.render(file, config).then(done, nope);
+    });
 }
 
 module.exports = {
@@ -40,15 +57,16 @@ module.exports = {
         'less'
     ],
     files: [
-        'app/src/**/*.{html,css,less,js}',
+        'app/src/**/*.{html,json,js}',
         'app/require/*.js',
         'index.html', {
-            match: '**/*.less',
+            match: '**/*.{css,less}',
             options: {
                 ignoreInitial: true
             },
             fn: function(event, file) {
                 if (event === 'change') {
+                    console.log('LESS file changed:', file);
                     this.reload('app/src/main.less');
                 }
             }
