2339be38fe0f3caea4c971f371f56651c31125b2
Kevin James <kevinjamesus86@gmail.com>
2016-03-27 18:49:09 -0700

    forked lite-server
    having the Browsersync instance exposed means we can send notifications
    to the client and such

--- bs-config.js
+++ bs-config.js
@@ -1,81 +1,95 @@
-'use strict';
+module.exports = function(bs) {
+    'use strict';
 
-var fs = require('fs'),
-    url = require('url'),
-    less = require('less'),
-    chalk = require('chalk');
+    var fs = require('fs'),
+        url = require('url'),
+        less = require('less'),
+        chalk = require('chalk'),
+        config;
 
-/**
- * Run the middleware on files that contain .less
- */
-function lessMiddleware(req, res, next) {
-    var parsed = url.parse(req.url);
-    if (parsed.pathname.match(/\.less$/)) {
-        parseLess(parsed.pathname, function(o) {
-            res.setHeader('Content-Type', 'text/css');
-            res.end(o.css);
-        });
-    } else {
-        next && next();
-    }
-}
-
-/**
- * Compile less
- */
-function parseLess(src, done) {
-    var config = {
-        paths: [
-            'app/src',
-            'app/components'
-        ]
-    };
-
-    function nope(err) {
-        var msg =
-            '\n' +
-            chalk.red('LESS Compilation Error ') +
-            chalk.grey(JSON.stringify(err, null, 2)) +
-            '\n';
-
-        console.log(msg);
-
-        done({
-            css:`/** ${chalk.stripColor(msg)} */`
-        });
-    }
-
-    fs.readFile('./' + src, 'utf-8', function(e, file) {
-        less.render(file, config).then(done, nope);
-    });
-}
-
-module.exports = {
-    injectChanges: true,
-    injectFileTypes: [
-        'css',
-        'less'
-    ],
-    files: [
-        'app/src/**/*.{html,json,js}',
-        'app/require/*.js',
-        'index.html', {
-            match: '**/*.{css,less}',
-            options: {
-                ignoreInitial: true
-            },
-            fn: function(event, file) {
-                if (event === 'change') {
-                    console.log('LESS file changed:', file);
-                    this.reload('app/src/main.less');
+    config = {
+        injectChanges: true,
+        injectFileTypes: [
+            'css',
+            'less'
+        ],
+        files: [
+            'app/src/**/*.{html,json,js}',
+            'app/require/*.js',
+            'index.html', {
+                match: '**/*.{css,less}',
+                options: {
+                    ignoreInitial: true
+                },
+                fn: function(event, file) {
+                    if (event === 'change') {
+                        console.log('LESS file changed:', file);
+                        this.reload('app/src/main.less');
+                    }
                 }
             }
+        ],
+        server: {
+            index: 'app/src/index.html',
+            middleware: {
+                2: lessMiddleware
+            }
         }
-    ],
-    server: {
-        index: 'app/src/index.html',
-        middleware: {
-            2: lessMiddleware
+    };
+
+    return config;
+
+    ///////////////////
+    ///////////////////
+
+    /**
+     * Run the middleware on files that contain .less
+     */
+    function lessMiddleware(req, res, next) {
+        var parsed = url.parse(req.url);
+        if (parsed.pathname.match(/\.less$/)) {
+            parseLess(parsed.pathname, function(o) {
+                res.setHeader('Content-Type', 'text/css');
+                res.end(o.css);
+            });
+        } else {
+            next && next();
         }
+    }
+
+    /**
+     * Compile less
+     */
+    function parseLess(src, done) {
+        var config = {
+            paths: [
+                'app/src',
+                'app/components'
+            ]
+        };
+
+        function nope(err) {
+            var msg =
+                '\n' +
+                chalk.red('LESS Compilation Error ') +
+                chalk.grey(JSON.stringify(err, null, 2)) +
+                '\n',
+                plain = chalk.stripColor(msg);
+
+            // drop it in the console
+            console.log(msg);
+
+            // tell the client about it
+            bs.notify('LESS compilation error, check yo terminal.', 5000);
+
+            // and send it in the source
+            done({
+                css: `/** ${plain} */`
+            });
+        }
+
+        fs.readFile('./' + src, 'utf-8', function(e, file) {
+            less.render(file, config).then(done, nope);
+        });
     }
 };
