b0242e90c3203620d0a07eedc416a69ee36944c1
Kevin James <kevinjamesus86@gmail.com>
2016-03-27 20:04:58 -0700

    the angular 1.5.x version

--- app/require/shims.js
+++ app/require/shims.js
@@ -1,1 +1,47 @@
-define({});
+define({
+    angular: {
+        exports: 'angular'
+    },
+    ngAria: {
+        deps: ['angular'],
+        init: function(angular) {
+            return angular.module('ngAria');
+        }
+    },
+    ngAnimate: {
+        deps: ['angular'],
+        init: function(angular) {
+            return angular.module('ngAnimate');
+        }
+    },
+    ngCookies: {
+        deps: ['angular'],
+        init: function(angular) {
+            return angular.module('ngCookies');
+        }
+    },
+    ngComponentRouter: {
+        deps: ['angular'],
+        init: function(angular) {
+            return angular.module('ngComponentRouter');
+        }
+    },
+    ngResource: {
+        deps: ['angular'],
+        init: function(angular) {
+            return angular.module('ngResource');
+        }
+    },
+    ngSanitize: {
+        deps: ['angular'],
+        init: function(angular) {
+            return angular.module('ngSanitize');
+        }
+    },
+    ngMocks: {
+        deps: ['angular'],
+        init: function(angular) {
+            return angular.module('ngMocks');
+        }
+    }
+});
--- app/src/main.js
+++ app/src/main.js
@@ -1,9 +1,36 @@
-define(function defineMain() {
+define([
+    'angular',
+    'ngComponentRouter'
+], function defineMain(
+    angular,
+    ngComponentRouter
+) {
     'use strict';
 
-    return {
-        strap: function() {
-            document.querySelector('#strap').textContent = 'Hey thur';
+    var module = angular.module('strap', [
+        ngComponentRouter.name
+    ]);
+
+    module.config([
+        '$compileProvider',
+        '$rootScopeProvider',
+    function($compileProvider, $rootScopeProvider) {
+        $compileProvider.debugInfoEnabled(false);
+        // this is sorta like a challenge
+        $compileProvider.onChangesTtl(5);
+        $rootScopeProvider.digestTtl(5);
+    }]);
+
+    ////////////////
+    // strap up main
+    ////////////////
+
+    module.component('main', {
+        template: '<h1>{{$ctrl.msg}}</h1>',
+        bindings: {
+            msg: '@'
         }
-    };
+    });
+
+    return module;
 });
